b2fc3bfaa30b0462502c082129360db9116aafe7
Jonas <jonascarpay@gmail.com>
2020-06-26 19:38:56 +0900

    Psuedo tests

--- test/Main.hs
+++ test/Main.hs
@@ -1,2 +1,12 @@
 main :: IO ()
 main = putStrLn "111"
+-- store a >> retrieve = store a >> store a >> retrieve    <- hard to check
+-- store a >>= retrieve = a
+--
+-- store a >> collectGarbage >> retrieve = error
+-- store a >> markRoot a >> collectGarbage >> retrieve = a
+--
+-- runCasperT act1 >> runCasperT act2
+-- runCasperT (act1 >> act2)
+--
+-- Something with references
