bd44c29849837bb7caf564ee9ec00989759e584f
Jonas <jonascarpay@gmail.com>
2020-07-01 14:03:15 +0900

    Cleaned up using `cabal-bounds`, `cabal-fmt`, `nixfmt`

--- default.nix
+++ default.nix
@@ -1,10 +1,11 @@
 let
   # haskellNix = import <haskell-nix> {};
   haskellNixSrc = builtins.fetchTarball {
-    url = "https://github.com/input-output-hk/haskell.nix/archive/fd60cf3f07d8e3c01feed62b69ea944c5fa067e7.tar.gz";
+    url =
+      "https://github.com/input-output-hk/haskell.nix/archive/fd60cf3f07d8e3c01feed62b69ea944c5fa067e7.tar.gz";
     sha256 = "090hmx3ppzbf2f59m9y9b8nvsrqbgrfvm1jj2630v6x72vh17wpa";
   };
-  haskellNix = import haskellNixSrc {};
+  haskellNix = import haskellNixSrc { };
   pkgsSrc = haskellNix.sources.nixpkgs-2003;
   pkgsArgs = haskellNix.nixpkgsArgs;
   pkgs = import pkgsSrc pkgsArgs;
--- shell.nix
+++ shell.nix
@@ -1,14 +1,12 @@
-let
-  hsPkgs = import ./.;
-in
-  hsPkgs.shellFor {
-    withHoogle = true;
-    tools = {
-      cabal = "3.2.0.0";
-      hlint = "3.1.5";
-      ghcid = "0.8.7";
-      ghcide = "0.2.0";
-      ormolu = "0.1.0.0";
-    };
-    exactDeps = true;
-  }
+let hsPkgs = import ./.;
+in hsPkgs.shellFor {
+  withHoogle = true;
+  tools = {
+    cabal = "3.2.0.0";
+    hlint = "3.1.5";
+    ghcid = "0.8.7";
+    ghcide = "0.2.0";
+    ormolu = "0.1.0.0";
+  };
+  exactDeps = true;
+}
