97b93660d795f4422e362834fe30059ab4485d7e
Ramin Honary <ramin.honary@cross-compass.com>
2022-06-09 17:26:50 +0900

    tests for 'openStore', 'transact', 'newVar', 'newRef', 'writeVar'

--- test/Main.hs
+++ test/Main.hs
@@ -1,86 +1,59 @@
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE DerivingVia #-}
 
-import Casper
-import Control.Exception
-import Internal (CasperError (..), Store (..), blobPath, hashBS)
+import qualified Casper
+
+import Control.Monad (unless)
+
+import Data.Aeson (FromJSON, ToJSON)
+import Data.ByteString.Char8 (ByteString)
+import Data.Serialize (Serialize)
+import GHC.Generics (Generic)
+
 import System.Directory
 import System.FilePath
-import System.IO.Temp
-import Test.QuickCheck
-import Test.QuickCheck.Monadic
+
 import Test.Tasty (TestTree, defaultMain, testGroup)
-import Test.Tasty.Hspec
-import Test.Tasty.QuickCheck
-import Control.Concurrent.STM.TVar(newTVarIO)
-import qualified Data.Set as Set
+import Test.Tasty.Hspec (Spec, testSpecs, it, example)
 
-spec :: Spec
-spec = describe "SHA256" $ do
-  it "should give path to content" $ do
-    lock <- newTVarIO False
-    activeBlocks <- newTVarIO 0
-    blobPath (hashBS "bar") (Store "foo" lock activeBlocks) `shouldBe` "foo/blob/_N4rLtula_QIYB-3If6bXDONEO5CnqBPrlURto-_j7k="
+----------------------------------------------------------------------------------------------------
 
-prop_reverse :: [Int] -> Bool
-prop_reverse xs = reverse (reverse xs) == xs
+type TestString = ByteString
 
-inTemporaryStore :: (Casper.Store -> IO b) -> IO b
-inTemporaryStore f = withSystemTempDirectory "temp" $ \tmp -> initStore (tmp </> "store") >>= f
+newtype TrivialTestType s = TrivialTestType [Casper.Ref TestString s]
+  deriving stock (Generic)
+  deriving (Casper.Content)
+  deriving anyclass (FromJSON, ToJSON)
+  deriving (Serialize) via Casper.WrapAeson (TrivialTestType s)
 
--- | Storing and then retrieving the stored item should always return the stored contents
--- >>> store a >>= retrieve = a
-prop_store_retreive :: Int -> Property
-prop_store_retreive a = monadicIO $ run $ inTemporaryStore $ \root -> do
-  result <- runCasperT root $ store a >>= retrieve
-  pure $ result === Right a
+initTrivial :: TrivialTestType s
+initTrivial = TrivialTestType []
 
--- | This test transitively tests that:
--- >>> store a >>= retrieve = store a >> store a >>= retrieve
--- given 'prop_store_retrieve'
-prop_store_store_retreive :: Int -> Property
-prop_store_store_retreive a = monadicIO $ run $ inTemporaryStore $ \root -> do
-  result <- runCasperT root $ store a >> store a >>= retrieve
-  pure $ result === Right a
+testStorePath :: FilePath
+testStorePath = "./casper-test-store"
 
--- | Retrieving an address after collecting garbage with no roots should yield an error
--- >>> store a >> collectGarbage >> retrieve = Left _
-prop_retrive_after_colllect_garbage :: Int -> Property
-prop_retrive_after_colllect_garbage a = monadicIO $ run $ inTemporaryStore $ \root -> do
-  Right addr <- runCasperT root $ store a
-  collectGarbage root (pure mempty)
-  result <- runCasperT root $ retrieve addr
-  pure $ result === Left (FileMissing (blobPath sha root))
-  where
-    sha = hashBS (encodeContent a)
-
--- | If the stored address is marked as a root it should not get removed
--- store a >> markRoot a >> collectGarbage >> retrieve = a
-prop_retrive_after_colllect_garbage_with_root :: Int -> Property
-prop_retrive_after_colllect_garbage_with_root a = monadicIO $ run $ inTemporaryStore $ \root -> do
-  Right addr <- runCasperT root $ store a
-  collectGarbage root (pure (Set.singleton (forget addr)))
-  result <- runCasperT root $ retrieve addr
-  pure $ result === Right a
-
--- How do generate an arbitrary CasperT m a action?
--- runCasperT store act1 >> runCasperT store act2 = runCasperT store (act1 >> act2)
-
--- Test something with references
-
--- Inject quickcheck properties here
-return []
-
-quickTest :: TestTree
-quickTest = testProperties "quickcheck" $allProperties
+simpleTests :: Spec
+simpleTests = do
+  let hello = "Hello, world!" :: TestString
+  it ("opens a new content addressable store, stores a single value " <> show hello) $
+    example $ do
+      (root, store) <- Casper.openStore testStorePath initTrivial $ \ root -> do
+        root <- Casper.transact $ Casper.newVar root
+        store <- Casper.getStore
+        pure (root, store)
+      Casper.runCasperT store $
+        Casper.transact $ do
+          ref <- Casper.newRef hello
+          (TrivialTestType refList) <- Casper.readVar root
+          Casper.writeVar root $ TrivialTestType $ ref:refList
 
 main :: IO ()
-main = do
-  specTest <- testSpec "spec" spec
+main =
+  testGroup "Casper" <$>
+  sequence
+  [ testGroup "Simple Tests" <$> testSpecs simpleTests
+  ] >>=
   defaultMain
-    ( testGroup
-        "tests"
-        [ specTest,
-          quickTest
-        ]
-    )
